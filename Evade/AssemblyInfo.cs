4647e77ace3d148303b8af2873e26068d926e6d9
Kyn9x <h.kyn9x@gmail.com>
2016-06-03 03:10:11 +0700

    Update AssemblyInfo.cs

--- Evade/AssemblyInfo.cs
+++ Evade/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.10.22")]
-[assembly: AssemblyFileVersion("6.10.22")]
+[assembly: AssemblyVersion("6.10.23")]
+[assembly: AssemblyFileVersion("6.10.23")]
