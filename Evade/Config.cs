b465109234e656291840aa91ab9e070be8c138c2
Kyn9x <h.kyn9x@gmail.com>
2016-06-03 01:47:28 +0700

    Update Config.cs

--- Evade/Config.cs
+++ Evade/Config.cs
@@ -42,12 +42,12 @@ namespace Evade
         public const int DiagonalEvadePointsCount = 7;
         public const int DiagonalEvadePointsStep = 20;
 
-        public const int CrossingTimeOffset = 200;
+        public const int CrossingTimeOffset = 170;
 
-        public const int EvadingFirstTimeOffset = 200;
-        public const int EvadingSecondTimeOffset = 30;
+        public const int EvadingFirstTimeOffset = 170;
+        public const int EvadingSecondTimeOffset = 0;
 
-        public const int EvadingRouteChangeTimeOffset = 200;
+        public const int EvadingRouteChangeTimeOffset = 170;
 
         public const int EvadePointChangeInterval = 300;
         public static int LastEvadePointChangeT = 0;
