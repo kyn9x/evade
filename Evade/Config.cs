28effc81a2998d438e3f7669cccd05574befe9aa
Kyn9x <h.kyn9x@gmail.com>
2016-06-03 06:25:43 +0700

    Update Config.cs

--- Evade/Config.cs
+++ Evade/Config.cs
@@ -40,7 +40,7 @@ namespace Evade
         public const int PathFindingDistance2 = 35;
 
         public const int DiagonalEvadePointsCount = 250;
-        public const int DiagonalEvadePointsStep = 10;
+        public const int DiagonalEvadePointsStep = 0;
 
         public const int CrossingTimeOffset = 0;
 
