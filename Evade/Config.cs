205a1908598ff0501f5f8523d03e050b0c897084
Kyn9x <h.kyn9x@gmail.com>
2016-06-06 21:55:22 +0700

    Update Config.cs

--- Evade/Config.cs
+++ Evade/Config.cs
@@ -32,10 +32,10 @@ namespace Evade
     {
         public const bool PrintSpellData = false;
         public const bool TestOnAllies = false;
-        public const int SkillShotsExtraRadius = 9;
-        public const int SkillShotsExtraRange = 20;
+        public const int SkillShotsExtraRadius = 15;
+        public const int SkillShotsExtraRange = 26;
         public const int GridSize = 10;
-        public const int ExtraEvadeDistance = 15;
+        public const int ExtraEvadeDistance = 25;
         public const int PathFindingDistance = 70;
         public const int PathFindingDistance2 = 70;
 
