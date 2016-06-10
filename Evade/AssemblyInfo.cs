ab00e8b7a5efa2a8837170e271428164ffb04f28
Kyn9x <h.kyn9x@gmail.com>
2016-06-11 03:39:27 +0700

    Update Taliyah

--- Evade/AssemblyInfo.cs
+++ Evade/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.10.23")]
-[assembly: AssemblyFileVersion("6.10.23")]
+[assembly: AssemblyVersion("6.11.1")]
+[assembly: AssemblyFileVersion("6.11.1")]
