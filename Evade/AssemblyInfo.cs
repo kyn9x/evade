ce5e3a46818864400c5a26c06b3a505dc904da3f
Kyn9x <h.kyn9x@gmail.com>
2016-06-17 12:31:44 +0700

    fix zileanQ

--- Evade/AssemblyInfo.cs
+++ Evade/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.12.0")]
-[assembly: AssemblyFileVersion("6.12.0")]
+[assembly: AssemblyVersion("6.12.1")]
+[assembly: AssemblyFileVersion("6.12.1")]
