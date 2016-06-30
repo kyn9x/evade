af9229e9efe94d1b4c5fc166806bb04eb57b98cb
Kyn9x <h.kyn9x@gmail.com>
2016-07-01 06:05:41 +0700

    Update Jinx.cs

--- AutoBuddy_BETA_Fixed/MyChampLogic/Jinx.cs
+++ AutoBuddy_BETA_Fixed/MyChampLogic/Jinx.cs
@@ -28,7 +28,7 @@ namespace AutoBuddy.MyChampLogic
 
             skillSequence = new[] { 1, 3, 2, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3 };
             ShopSequence =
-                "3340:Buy,2003:StartHpPot,1055:Buy,3086:Buy,3087:Buy,1001:Buy,1053:Buy,3144:Buy,1043:Buy,3153:Buy,1038:Buy,2003:StopHpPot,1037:Buy,3031:Buy,3006:Buy,1038:Buy,1055:Sell,1053:Buy,3072:Buy,3140:Buy,3139:Buy";
+                "3340:Buy,1055:Buy,2003:StartHpPot,1038:Buy,3133:Buy,3508:Buy,3086:Buy,1001:Buy,1001:Buy,3006:Buy,1038:Buy,3072:Buy,2003:StopHpPot,1038:Buy,3031:Buy,1055:Sell,3035:Buy,3036:Buy";
             Game.OnTick += Game_OnTick;
         }
 
