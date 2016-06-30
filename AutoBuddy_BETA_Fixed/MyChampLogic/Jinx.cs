6dbf72165ff7f1fb42ab6da140d2b984daa61ca2
Kyn9x <h.kyn9x@gmail.com>
2016-07-01 06:17:05 +0700

    Update Jinx.cs

--- AutoBuddy_BETA_Fixed/MyChampLogic/Jinx.cs
+++ AutoBuddy_BETA_Fixed/MyChampLogic/Jinx.cs
@@ -20,7 +20,7 @@ namespace AutoBuddy.MyChampLogic
         {
             skillSequence = new[] { 1, 3, 2, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3 };
             ShopSequence =
-                "3340:Buy,1055:Buy,2003:StartHpPot,1038:Buy,3133:Buy,3508:Buy,3086:Buy,1001:Buy,3006:Buy,1038:Buy,3072:Buy,2003:StopHpPot,1038:Buy,3031:Buy,1055:Sell,3035:Buy,3036:Buy";
+                "3340:Buy,1055:Buy,2003:StartHpPot,1038:Buy,3133:Buy,3508:Buy,3086:Buy,3085:Buy,1001:Buy,3006:Buy,1038:Buy,3072:Buy,2003:StopHpPot,1038:Buy,3031:Buy,1055:Sell,3035:Buy,3036:Buy";
         }
 
         public int[] skillSequence { get; private set; }
