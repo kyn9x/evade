c05593aa29b665ec5b166b0d274a09677747e6a2
Kyn9x <h.kyn9x@gmail.com>
2016-07-03 00:56:01 +0700

    Update Config.cs

--- Evade/Config.cs
+++ Evade/Config.cs
@@ -36,11 +36,11 @@ namespace Evade
         public const int SkillShotsExtraRange = 20;
         public const int GridSize = 10;
         public const int ExtraEvadeDistance = 15;
-        public const int PathFindingDistance = 75;
+        public const int PathFindingDistance = 180;
         public const int PathFindingDistance2 = 35;
 
         public const int DiagonalEvadePointsCount = 7;
-        public const int DiagonalEvadePointsStep = 20;
+        public const int DiagonalEvadePointsStep = 70;
 
         public const int CrossingTimeOffset = 250;
 
