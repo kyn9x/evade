c40ff8916cd3866848f30130a2da3b3d15296140
Kyn9x <h.kyn9x@gmail.com>
2016-07-17 04:26:44 +0700

    Update Config.cs

--- Evade/Config.cs
+++ Evade/Config.cs
@@ -158,7 +158,7 @@ namespace Evade
             drawings.Add("ShowEvadeStatus", new CheckBox("Draw Evade Status"));
             
             misc = Menu.AddSubMenu("Misc", "Misc");
-            misc.AddStringList("BlockSpells", "Block spells while evading", new[] { "No", "Only dangerous", "Always" }, 1);
+            misc.AddStringList("BlockSpells", "Block spells while evading", new ComboBox("No", 2, "Only Dangerous", "Always"))
             //misc.Add("BlockSpells", "Block spells while evading").SetValue(new StringList(new []{"No", "Only dangerous", "Always"}, 1)));
             misc.Add("DisableFow", new CheckBox("Disable FOW dodging", false));
             misc.Add("ShowEvadeStatus", new CheckBox("Show Evade Status"));
