d9867a2f986611b4408c6e8e07a89442dca6a251
Kyn9x <h.kyn9x@gmail.com>
2016-07-22 05:29:22 +0700

    Add files via upload

---
+++ MoonWalkEvade/Program.cs
@@ -0,0 +1,27 @@
+﻿using EloBuddy.SDK.Events;
+using MoonWalkEvade.Skillshots;
+using MoonWalkEvade.Utils;
+using Collision = MoonWalkEvade.Evading.Collision;
+
+namespace MoonWalkEvade
+{
+    internal static class Program
+    {
+        public static bool DeveloperMode = false;
+
+        private static SpellDetector _spellDetector;
+
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += delegate
+            {
+                _spellDetector = new SpellDetector(DeveloperMode ? DetectionTeam.AnyTeam : DetectionTeam.EnemyTeam);
+                new Evading.MoonWalkEvade(_spellDetector);
+                EvadeMenu.CreateMenu();
+
+                Collision.Init();
+                Debug.Init(ref _spellDetector);
+            };
+        }
+    }
+}
---
+++ MoonWalkEvade/Skillshots/SkillshotTypes/YasuoQ.cs
@@ -0,0 +1,20 @@
+﻿using EloBuddy;
+using MoonWalkEvade.Utils;
+
+namespace MoonWalkEvade.Skillshots.SkillshotTypes
+{
+    public class YasuoQ : LinearSkillshot
+    {
+        public override EvadeSkillshot NewInstance(bool debug = false)
+        {
+            var newInstance = new YasuoQ { OwnSpellData = OwnSpellData };
+            return newInstance;
+        }
+
+        public override void OnSpellDetection(Obj_AI_Base sender)
+        {
+            _startPos = Caster.ServerPosition;
+            _endPos = _startPos.ExtendVector3(CastArgs.End, -OwnSpellData.Range);
+        }
+    }
+}
