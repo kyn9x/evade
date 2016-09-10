87d2ac630469aafcdadeeaf12c125f36407df117
Kyn9x <h.kyn9x@gmail.com>
2016-09-11 04:52:44 +0700

    Update Program.cs

--- Evade/Program.cs
+++ Evade/Program.cs
@@ -144,10 +144,10 @@ namespace Evade
             //Initialze the collision
             Collision.Init();
 
-            Chat.Print("Evade By Kyn", Color.Red);
+            /*Chat.Print("Evade By Kyn", Color.Red);
             Chat.Print("Update 6.17: 30/08/2016", Color.Green);
             Console.WriteLine("Evade:: Evade By Kyn");
-            Console.WriteLine("Evade::Update 6.17: 30/08/2016");
+            Console.WriteLine("Evade::Update 6.17: 30/08/2016");*/
 
             if (Config.PrintSpellData)
             {
